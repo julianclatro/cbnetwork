f16c5658c439c6a308f3a1842666e3a86159a9ec
Julian Clatro <jclatro@yahoo.com>
2020-03-12 13:13:47 -0300

    almost there bro

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-activate :i18n, :path => "/:locale/"
+activate :i18n, :path => "/:locale/", :mount_at_root => :es
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
